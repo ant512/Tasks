33fd337c6170a47ef5fc884036a68b91ba7ab3a1
ant512 <devnull@localhost>
2011-01-11 16:04:11 +0000

    Added comments.

--- source/Tasks/Dependencies/FinishToStartDependency.cs
+++ source/Tasks/Dependencies/FinishToStartDependency.cs
@@ -6,15 +6,39 @@ using WorkingWeek;
 
 namespace Tasks
 {
+	/// <summary>
+	/// Defines a "finish to start dependency", in which task B (owner)
+	/// will start when task A (dependent on) ends.  This is a low priority
+	/// dependency.
+	/// </summary>
 	public class FinishToStartDependency : Dependency
 	{
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the FinishToStartDependency class.
+		/// </summary>
+		/// <param name="dependentOn">The task on which the dependency is dependent.</param>
+		/// <param name="lag">The lag of the dependency.</param>
 		public FinishToStartDependency(ITask dependentOn, TimeSpan lag) : base(dependentOn, DependencyPriority.Low, lag)
 		{
 		}
 
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Gets the start date of the dependency, using the supplied working week
+		/// definition to identify working vs. non-working times.
+		/// </summary>
+		/// <param name="week">The working week definition.</param>
+		/// <returns>The start date of the dependency.</returns>
 		public override DateTime StartDate(Week week)
 		{
 			return week.DateAdd(DependentOn.EndDate, Lag);
 		}
+
+		#endregion
 	}
 }
