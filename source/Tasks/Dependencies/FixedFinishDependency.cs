5a0fb305ce0386d158d8ce5f55739361286d66bd
ant512 <devnull@localhost>
2011-01-11 16:09:45 +0000

    Added comments.

--- source/Tasks/Dependencies/FixedFinishDependency.cs
+++ source/Tasks/Dependencies/FixedFinishDependency.cs
@@ -6,19 +6,49 @@ using WorkingWeek;
 
 namespace Tasks
 {
+	/// <summary>
+	/// Defines a "finished finish dependency", in which the task will
+	/// finish on a user-specified date.  This is a high priority
+	/// dependency.
+	/// </summary>
 	public class FixedFinishDependency : Dependency
 	{
+		#region Members
+
+		/// <summary>
+		/// The date on which the dependency will end.
+		/// </summary>
 		private DateTime mEndDate;
 
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the FixedFinishDependency class.
+		/// </summary>
+		/// <param name="endDate">The date on which the dependency will end.</param>
 		public FixedFinishDependency(DateTime endDate)
 			: base(null, DependencyPriority.High, new TimeSpan(0))
 		{
 			mEndDate = endDate;
 		}
 
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Gets the start date of the dependency, using the supplied working week
+		/// definition to identify working vs. non-working times.
+		/// </summary>
+		/// <param name="week">The working week definition.</param>
+		/// <returns>The start date of the dependency.</returns>
 		public override DateTime StartDate(Week week)
 		{
 			return week.DateAdd(mEndDate, Owner.Duration.Negate());
 		}
+
+		#endregion
 	}
 }
--- source/Tasks/Dependencies/FixedStartDependency.cs
+++ source/Tasks/Dependencies/FixedStartDependency.cs
@@ -6,19 +6,49 @@ using WorkingWeek;
 
 namespace Tasks
 {
+	/// <summary>
+	/// Defines a "finished start dependency", in which the task will
+	/// start on a user-specified date.  This is a high priority
+	/// dependency.
+	/// </summary>
 	public class FixedStartDependency : Dependency
 	{
+		#region Members
+
+		/// <summary>
+		/// The date on which the dependency will start.
+		/// </summary>
 		private DateTime mStartDate;
 
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the FinishToStartDependency class.
+		/// </summary>
+		/// <param name="startDate">The date on which the dependency will start.</param>
 		public FixedStartDependency(DateTime startDate)
 			: base(null, DependencyPriority.High, new TimeSpan(0))
 		{
 			mStartDate = startDate;
 		}
 
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Gets the start date of the dependency, using the supplied working week
+		/// definition to identify working vs. non-working times.
+		/// </summary>
+		/// <param name="week">The working week definition.</param>
+		/// <returns>The start date of the dependency.</returns>
 		public override DateTime StartDate(Week week)
 		{
 			return mStartDate;
 		}
+
+		#endregion
 	}
 }
