5a01be906d3a27e7ee1220c874f905a11c293f9a
ant512 <devnull@localhost>
2011-01-11 16:12:19 +0000

    Added comments.

--- source/Tasks/Dependencies/StartToStartDependency.cs
+++ source/Tasks/Dependencies/StartToStartDependency.cs
@@ -6,16 +6,40 @@ using WorkingWeek;
 
 namespace Tasks
 {
+	/// <summary>
+	/// Defines a "start to start dependency", in which task A (dependent on)
+	/// and task B (owner) will start simultaneously.  This is a low priority
+	/// dependency.
+	/// </summary>
 	public class StartToStartDependency : Dependency
 	{
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the StartToStartDependency class.
+		/// </summary>
+		/// <param name="dependentOn">The task on which the dependency is dependent.</param>
+		/// <param name="lag">The lag of the dependency.</param>
 		public StartToStartDependency(ITask dependentOn, TimeSpan lag)
 			: base(dependentOn, DependencyPriority.Low, lag)
 		{
 		}
 
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Gets the start date of the dependency, using the supplied working week
+		/// definition to identify working vs. non-working times.
+		/// </summary>
+		/// <param name="week">The working week definition.</param>
+		/// <returns>The start date of the dependency.</returns>
 		public override DateTime StartDate(Week week)
 		{
 			return week.DateAdd(DependentOn.StartDate, Lag);
 		}
+
+		#endregion
 	}
 }
